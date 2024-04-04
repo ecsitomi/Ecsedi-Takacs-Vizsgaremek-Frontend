27a8bdc41325aa253976fc690a676dd49abf273c
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2024-04-04 18:49:08 +0200

    User token vizsgálat

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,8 +1,89 @@
 import '../App.css'
 
 function UserPage() {
+  const apiUrl = "http://localhost:8000/api";
+  const [user, setUser] = useState(null); //user állapotában tároljuk a tokent
 
-  return (<></>)
+  //Felhasználó adatainak letöltése
+  const loadUserData = async () => {
+    const token = localStorage.getItem("token");
+    const url = apiUrl + "/user";
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+      },
+    });
+    //Válasz
+    const data = await response.json();
+    if (response.ok) {
+      setUser(data);
+    } else {
+      localStorage.removeItem("token");
+    }
+  };
+
+  //Van-e bejelentkezett felhasználó? Ha igen, akkor betölti az adatait az oldal indulásakor
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      loadUserData();
+    } else {
+      setUser(null);
+    }
+  }, []);
+
+  //Kijelentkezés erről az eszközről
+  const logout = async () => {
+    const token = localStorage.getItem("token");
+    const url = apiUrl + "/logout";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+      },
+    });
+    //Válasz
+    if (response.ok) {
+      localStorage.removeItem("token");
+    } else {
+      const data = await response.json();
+      alert(data.message);
+    }
+  };
+
+  //Kijelentkezés minden eszközről
+  const logoutEverywhere = async () => {
+    const token = localStorage.getItem("token");
+    const url = apiUrl + "/logout-everywhere";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+      },
+    });
+    //Válasz
+    if (response.ok) {
+      localStorage.removeItem("token");
+    } else {
+      const data = await response.json();
+      alert(data.message);
+    }
+  };
+
+  //Megjelenítés
+  return user ? (
+    <div>
+      <p>Bejelentkezve: {user.name}</p>
+      <button type="button" onClick={() => logout()}>Kijelentkezés</button>
+      <button type="button" onClick={() => logoutEverywhere()}>Kijelentkezés mindenhonnan</button>
+    </div>
+  ) : (
+    <p>Nincs bejelentkezve senki!</p>
+  );
 }
 
 export default UserPage
