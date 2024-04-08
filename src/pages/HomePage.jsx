40f935b573ab57b4f34059534e9201073e53e08c
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2024-04-08 22:33:28 +0200

    kezdőlap hangolás

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,20 +6,21 @@ function HomePage() {
     // useEffect callback
   }, []);
 
-  return (<>
-    <div className="container">
+  return (
+    <>
+      <div className="container">
           <video className="fullscreen-video" autoPlay muted loop>
-            <source src="https://v1.cdnpk.net/videvo_files/video/free/video0488/large_preview/_import_629ee34561aa87.39478399.mp4" type="video/mp4" />
+            <source src="https://static.videezy.com/system/resources/previews/000/054/799/original/Shake-hand-close-up.mp4" type="video/mp4" />
           </video>
-      <div className="content">
-        <div className="text">
-          <br></br>
-          <h3 className="animate__animated animate__heartBeat">Ön bejelenti</h3>
-          <h3 className="animate__animated animate__backInUp">Mi megoldjuk</h3>
-          <br></br>
+        <div className="content">
+          <div className="text">
+            <br />
+            <h3 className="animate__animated animate__heartBeat">Ön bejelenti</h3>
+            <h3 className="animate__animated animate__backInUp">Mi megoldjuk</h3>
+            <br />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
